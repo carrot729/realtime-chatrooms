77438b93bda5db46bf28cd82f23d595ab99ba2fc
DefNotArham <arhamkabir231@gmail.com>
2026-07-06 20:06:24 -0400

    instaleld cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import connectDb from "./db/connectDb.js";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
+  connectDb();
   console.log(`Server is running on ${PORT}`);
 });
