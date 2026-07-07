a5e937252dbfaf710927a889d5f2f716ebb2448c
DefNotArham <arhamkabir231@gmail.com>
2026-07-06 20:30:27 -0400

    created homepage with chatgpt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div></div>;
-};
+import { Routes, Route } from "react-router";
+
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+}
 
 export default App;
