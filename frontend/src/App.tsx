c33bff4affbedd2ee2250bbfde9b2c8288716fc9
DefNotArham <arhamkabir231@gmail.com>
2026-07-08 19:37:40 -0400

    create user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,25 @@ import { Routes, Route } from "react-router";
 
 import HomePage from "./pages/HomePage";
 
+import useUserStore from "./stores/user.store";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const initUser = async () => {
+      let clientId = localStorage.getItem("clientId");
+
+      if (!clientId) {
+        clientId = crypto.randomUUID();
+        localStorage.setItem("clientId", clientId);
+      }
+
+      await useUserStore.getState().createUser(clientId);
+    };
+
+    initUser();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
