9819bd480b4fe7b132279ab8a33bdab81ec0a86a
DefNotArham <arhamkabir231@gmail.com>
2026-07-08 19:32:56 -0400

    created userstore

--- frontend/src/stores/user.store.ts
+++ frontend/src/stores/user.store.ts
@@ -1,3 +1,19 @@
 import { create } from "zustand";
 
-const useUserStore = create(() => ({}));
+import api from "../lib/api.ts";
+
+type UserStoreType = {
+  createNewUser: (clientId: string) => Promise<void>;
+};
+
+const useUserStore = create(() => ({
+  createUser: async (clientId: string) => {
+    try {
+      await api.post("/user/create-user", { clientId });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}));
+
+export default useUserStore;
