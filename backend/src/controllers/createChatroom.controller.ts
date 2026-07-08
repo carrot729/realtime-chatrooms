171041a0c5f7c8b253ac91270d14245ce8168fbb
DefNotArham <arhamkabir231@gmail.com>
2026-07-08 18:33:12 -0400

    instaleld tailwindvite

--- backend/src/controllers/createChatroom.controller.ts
+++ backend/src/controllers/createChatroom.controller.ts
@@ -1,3 +1,13 @@
-const createChatroomController = async () => {};
+import Chatroom from "../models/chatroom.model.js";
+import User from "../models/user.model.js";
+import Message from "../models/messsage.model.js";
+
+import type { Request, Response } from "express";
+
+const createChatroomController = async (req: Request, res: Response) => {
+  const {} = req.body;
+  try {
+  } catch (error) {}
+};
 
 export default createChatroomController;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,27 +1,15 @@
 import "./HomePage.css";
 
-const rooms = [
-  { id: 1, name: "General Chat" },
-  { id: 2, name: "Gaming" },
-  { id: 3, name: "Programming" },
-];
-
 const HomePage = () => {
   return (
     <div className="home">
       <div className="container">
         <div className="header">
           <h1>Socket.IO Chat</h1>
-          <button className="create-room-btn">+ Create Room</button>
-        </div>
-
-        <div className="rooms">
-          {rooms.map((room) => (
-            <div key={room.id} className="room-card">
-              <h2>{room.name}</h2>
-              <button className="join-btn">Join Room</button>
-            </div>
-          ))}
+          <div>
+            <button className="create-room-btn">+ Create Room</button>
+            <button className="create-room-btn">+ Create Room</button>
+          </div>
         </div>
       </div>
     </div>
