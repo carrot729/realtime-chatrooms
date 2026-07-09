be70b0fc45420eb9d71092029ced6f6359b4f67b
DefNotArham <arhamkabir231@gmail.com>
2026-07-08 21:19:01 -0400

    fixed laod rooms

--- backend/src/controllers/loadRooms.controller.ts
+++ backend/src/controllers/loadRooms.controller.ts
@@ -2,29 +2,32 @@ import type { Request, Response } from "express";
 
 import User from "../models/user.model.js";
 
-type RoomType = {
+type RoomQuery = {
   clientId: string;
 };
 
 const loadRoomsController = async (
-  req: Request<{}, {}, RoomType>,
+  req: Request<{}, {}, {}, RoomQuery>,
   res: Response,
 ) => {
-  const { clientId } = req.body;
+  const { clientId } = req.query;
+
   try {
-    if (!clientId)
+    if (!clientId || typeof clientId !== "string")
       return res
         .status(404)
         .json({ success: false, message: "Client Id not found" });
 
-    const user = await User.findOne({ clientId });
+    const user = await User.findOne({ clientId }).populate("rooms");
+
     if (!user)
       return res
         .status(404)
         .json({ success: false, message: "User not found" });
 
-    res.status(200).json({ success: true, rooms: user.rooms });
+    return res.status(200).json({ success: true, rooms: user.rooms });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ success: false, message: "Server error" });
   }
 };
